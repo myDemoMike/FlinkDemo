97084487dc5083c4161c7e30a01e62d3a468a585
刘源 <898197688@qq.com>
2019-06-16 23:31:57 +0800

    flink batch

--- src/main/scala/com/my/base/FlinkBatchDemo.scala
+++ src/main/scala/com/my/base/FlinkBatchDemo.scala
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.{ProcessingTimeSessionWindows, SlidingEventTimeWindows, TumblingEventTimeWindows}
 
 object FlinkBatchDemo {
 
@@ -17,27 +18,40 @@ object FlinkBatchDemo {
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-//    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-//    env.enableCheckpointing(1000)
-//    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-//
-//    // configure Kafka consumer
-//    val kafkaProps = new Properties()
-//    kafkaProps.setProperty("zookeeper.connect", ZOOKEEPER_HOST)
-//    kafkaProps.setProperty("bootstrap.servers", KAFKA_BROKER)
-//    kafkaProps.setProperty("group.id", TRANSACTION_GROUP)
-//
-//    //topicd的名字是test，schema默认使用SimpleStringSchema()即可
-//    val myConsumer = new FlinkKafkaConsumer[String]("test", new SimpleStringSchema(), kafkaProps)
-//    val stream = env
-//      .addSource(myConsumer)
-    val stream = env.socketTextStream("10.31.1.122",9999)
-    val  output = stream.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }//nonEmpty非空的
-      .map { (_, 1) }
-      .keyBy(0)//通过Tuple的第一个元素进行分组
-      .timeWindow(Time.seconds(5))//Windows 根据某些特性将每个key的数据进行分组 (例如:在5秒内到达的数据).
+    //    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    //    env.enableCheckpointing(1000)
+    //    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    //
+    //    // configure Kafka consumer
+    //    val kafkaProps = new Properties()
+    //    kafkaProps.setProperty("zookeeper.connect", ZOOKEEPER_HOST)
+    //    kafkaProps.setProperty("bootstrap.servers", KAFKA_BROKER)
+    //    kafkaProps.setProperty("group.id", TRANSACTION_GROUP)
+    //
+    //    //topicd的名字是test，schema默认使用SimpleStringSchema()即可
+    //    val myConsumer = new FlinkKafkaConsumer[String]("test", new SimpleStringSchema(), kafkaProps)
+    //    val stream = env
+    //      .addSource(myConsumer)
+    val stream = env.socketTextStream("192.168.72.140", 9999)
+    val output = stream.flatMap {
+      _.toLowerCase.split("\\W+") filter {
+        _.nonEmpty
+      }
+    } //nonEmpty非空的
+      .map {
+      (_, 1)
+    }
+      .keyBy(0) //通过Tuple的第一个元素进行分组
+      //   每隔2秒中计算最近5秒的数据   等价于  SlidingEventTimeWindows
+      .timeWindow(Time.seconds(5), Time.seconds(2))
       .sum(1)
     output.print()
+
+
+
+
     env.execute()
+
+
   }
 }
---
+++ src/main/scala/com/my/base/WindowDemo.scala
@@ -0,0 +1,100 @@
+package com.my.base
+
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object WindowDemo {
+
+  private val ZOOKEEPER_HOST = "10.31.1.122:2181"
+  private val KAFKA_BROKER = "10.31.1.122:9092"
+  private val TRANSACTION_GROUP = "test"
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val stream = env.socketTextStream("192.168.72.140", 9999)
+    val output = stream.flatMap {
+      _.toLowerCase.split("\\W+") filter {
+        _.nonEmpty
+      }
+    } //nonEmpty非空的
+      .map {
+      (_, 1)
+    }
+      .keyBy(0) //通过Tuple的第一个元素进行分组
+      //   每隔2秒中计算最近5秒的数据   等价于  SlidingEventTimeWindows
+      .timeWindow(Time.seconds(5), Time.seconds(2))
+      .sum(1)
+
+    // 每隔5秒中的翻滚操作
+    //    val  output = stream.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
+    //      .map { (_, 1) }
+    //      .keyBy(0)
+    //      .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+    //      .sum(1)
+
+
+    // 每隔5秒中处理前10S的出局
+//    val output = stream.flatMap {
+//      _.toLowerCase.split("\\W+") filter {
+//        _.nonEmpty
+//      }
+//    }
+//      .map {
+//        (_, 1)
+//      }
+//      .keyBy(0)
+//      .window(SlidingEventTimeWindows.of(Time.seconds(10),Time.seconds(5)))
+//      .sum(1)
+
+
+//    val output = stream.flatMap {
+//      _.toLowerCase.split("\\W+") filter {
+//        _.nonEmpty
+//      }
+//    } //nonEmpty非空的
+//      .map {
+//      (_, 1)
+//    }
+//      .keyBy(0) //通过Tuple的第一个元素进行分组
+//      // 每当窗口中填满100的元素时，就会对窗口进行计算， 翻滚窗口
+//      .countWindow(100)
+//      .sum(1)
+
+//    val output = stream.flatMap {
+//      _.toLowerCase.split("\\W+") filter {
+//        _.nonEmpty
+//      }
+//    } //nonEmpty非空的
+//      .map {
+//      (_, 1)
+//    }
+//      .keyBy(0) //通过Tuple的第一个元素进行分组
+//      // 计算每10个元素计算一次最近100个元素的总和
+//      .countWindow(100,10)
+//      .sum(1)
+
+
+//    val output = stream.flatMap {
+//      _.toLowerCase.split("\\W+") filter {
+//        _.nonEmpty
+//      }
+//    } //nonEmpty非空的
+//      .map {
+//      (_, 1)
+//    }
+//      .keyBy(0) //通过Tuple的第一个元素进行分组
+//    // 如果用户30秒没有活动则视为会话断开（假设raw data stream是单个用户的购买行为流）
+//      .window(ProcessingTimeSessionWindows.withGap(Time.seconds(30)))
+//      .sum(1)
+    output.print()
+
+
+
+
+    env.execute()
+
+
+  }
+}
